669fd4ff8ea82c99f53483f1b0c61347c8d7d7d6
Isuru Hettiarachchi <isuru71@gmail.com>
2019-04-28 15:34:41 +0530

    Add AddReviewActivity

---
+++ app/src/main/java/com/ctse/androidgamereviewer/AddReviewActivity.java
@@ -0,0 +1,42 @@
+package com.ctse.androidgamereviewer;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class AddReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_close);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_review_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_review_button:
+                return true;
+            default:
+                finish();
+                return true;
+        }
+    }
+
+}
--- app/src/main/java/com/ctse/androidgamereviewer/ViewGameDetailsActivity.java
+++ app/src/main/java/com/ctse/androidgamereviewer/ViewGameDetailsActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,9 +27,9 @@ public class ViewGameDetailsActivity extends AppCompatActivity {
 
     // UI elements
     ImageView ivGameImage;
-    Button addReviewButton;
 
-    // TODO: Implement RecyclerView
+
+    // TODO: Implement RecyclerView for reviews
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +54,16 @@ public class ViewGameDetailsActivity extends AppCompatActivity {
                 tvGameTitle.setText(game.getTitle());
                 tvGameGenre.setText(game.getGenre());
                 tvGameReleaseDate.setText(game.getRelease_date());
+            }
+        });
+
+        Button addReviewButton = findViewById(R.id.button_add_review);
+        addReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ViewGameDetailsActivity.this, AddReviewActivity.class);
+                startActivity(intent);
+                // TODO: Start activity for result
             }
         });
 

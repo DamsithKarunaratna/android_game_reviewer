45d5d628f9c5807d1e6f4b277d017cb694594569
dammakaru <dammakaru@gmail.com>
2019-05-17 19:38:53 +0530

    Add authentication logic

--- app/src/main/java/com/ctse/androidgamereviewer/MainActivity.java
+++ app/src/main/java/com/ctse/androidgamereviewer/MainActivity.java
@@ -3,14 +3,23 @@ package com.ctse.androidgamereviewer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.ctse.androidgamereviewer.data.entities.Game;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -24,15 +33,56 @@ import org.bson.types.ObjectId;
 public class MainActivity extends AppCompatActivity {
 
     public static final int ADD_GAME_REQUEST = 2;
+    public static final int LOGIN_REQUEST = 1995;
 
     private GameViewModel gameViewModel;
     private ReviewViewModel reviewViewModel;
     SwipeRefreshLayout swipeRefreshLayout;
+    FirebaseUser user;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.login_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.login_menu_item:
+                openLoginActivity();
+                return true;
+            case R.id.logout_menu_item:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        System.out.println("On prepare called");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if(null==user) {
+            menu.findItem(R.id.logout_menu_item).setVisible(false);
+            menu.findItem(R.id.login_menu_item).setVisible(true);
+        } else {
+            menu.findItem(R.id.logout_menu_item).setVisible(true);
+            menu.findItem(R.id.login_menu_item).setVisible(false);
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
         swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -64,8 +114,22 @@ public class MainActivity extends AppCompatActivity {
         addGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddGameActivity.class);
-                startActivityForResult(intent, ADD_GAME_REQUEST);
+
+                // check if logged in
+                user = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (null!=user) {
+                    // Name, email address
+                    System.out.println(user.getDisplayName());
+                    System.out.println(user.getEmail());
+                    Intent intent = new Intent(MainActivity.this, AddGameActivity.class);
+                    startActivityForResult(intent, ADD_GAME_REQUEST);
+                } else {
+                    Toast.makeText(MainActivity.this, "you must be logged in",
+                            Toast.LENGTH_SHORT).show();
+                    openLoginActivity();
+                }
+
             }
         });
     }
@@ -95,10 +159,36 @@ public class MainActivity extends AppCompatActivity {
 
             Toast.makeText(this, "Game Saved", Toast.LENGTH_SHORT).show();
 
+        } else if (requestCode == LOGIN_REQUEST && resultCode == RESULT_OK) {
+            this.invalidateOptionsMenu();
+            this.supportInvalidateOptionsMenu();
         } else {
 
             Toast.makeText(this, "Game not saved", Toast.LENGTH_SHORT).show();
 
         }
     }
+
+    private void openLoginActivity() {
+        Intent intent = new Intent(MainActivity.this, SplashActivity.class);
+        startActivityForResult(intent, LOGIN_REQUEST);
+    }
+    private void logout() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "sign out successful",
+                                    Toast.LENGTH_SHORT).show();
+                            MainActivity.this.invalidateOptionsMenu();
+                            MainActivity.this.supportInvalidateOptionsMenu();
+                        } else {
+                            Toast.makeText(MainActivity.this, "sign out unsuccessful",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/ctse/androidgamereviewer/SplashActivity.java
+++ app/src/main/java/com/ctse/androidgamereviewer/SplashActivity.java
@@ -1,14 +1,33 @@
 package com.ctse.androidgamereviewer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+/*https://firebase.google.com/docs/auth/android/firebaseui */
 public class SplashActivity extends AppCompatActivity {
 
+    public static final int RC_SIGN_IN = 1994;
+
     Button loginButton;
+
+    // Choose FireBase authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +40,52 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 System.out.println("Login clicked");
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(),
+                        RC_SIGN_IN);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+
+                // Successfully signed in
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                setResult(RESULT_OK);
+                finish();
+//                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//
+//                if (null!=user) {
+//                    // Name, email address, and profile photo Url
+//                    System.out.println(user.getDisplayName());
+//                    System.out.println(user.getEmail());
+//                }
+
+            } else {
+
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                if(null!=response) {
+                    Toast.makeText(this, "Error in log in", Toast.LENGTH_SHORT).show();
+                    response.getError().printStackTrace();
+                } else {
+                    Toast.makeText(this, "Login canceled", Toast.LENGTH_SHORT).show();
+                }
+                setResult(RESULT_OK);
+                finish();
+
+            }
+        }
     }
 }

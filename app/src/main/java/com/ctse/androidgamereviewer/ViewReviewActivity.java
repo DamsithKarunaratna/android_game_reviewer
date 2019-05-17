7f90dbe08524325304d2620cd5b1e1cb5ee911a4
Isuru Hettiarachchi <isuru71@gmail.com>
2019-05-18 00:06:18 +0530

    UI changes to view review activity

--- app/src/main/java/com/ctse/androidgamereviewer/ViewReviewActivity.java
+++ app/src/main/java/com/ctse/androidgamereviewer/ViewReviewActivity.java
@@ -5,10 +5,15 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.ctse.androidgamereviewer.data.entities.Review;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ViewReviewActivity extends AppCompatActivity {
 
@@ -18,6 +23,11 @@ public class ViewReviewActivity extends AppCompatActivity {
     private TextView tvReviewTitle;
     private TextView tvReviewBody;
     private RatingBar ratingBar;
+    private LinearLayout linearLayout;
+    private Button buttonEditReivew;
+    private Button buttonDeleteReview;
+
+    FirebaseUser user;
 
     private String reviewId;
 
@@ -28,9 +38,14 @@ public class ViewReviewActivity extends AppCompatActivity {
 
         reviewId = getIntent().getStringExtra(ReviewViewAdapter.EXTRA_REVIEW_ID);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
         tvReviewTitle = findViewById(R.id.text_view_review_title_view);
         tvReviewBody = findViewById(R.id.text_view_review_body_view);
         ratingBar = findViewById(R.id.ratingBarReview);
+        linearLayout = findViewById(R.id.layout_options);
+        buttonEditReivew = findViewById(R.id.button_edit_review);
+        buttonDeleteReview = findViewById(R.id.button_delete_review);
 
         reviewViewModel = ViewModelProviders.of(this).get(ReviewViewModel.class);
         reviewViewModel.getReviewById(reviewId).observe(this, new Observer<Review>() {
@@ -39,7 +54,14 @@ public class ViewReviewActivity extends AppCompatActivity {
                 tvReviewBody.setText(review.getBody());
                 tvReviewTitle.setText(review.getTitle());
                 ratingBar.setRating(review.getRating());
+
+                if (null != user) {
+                    linearLayout.setVisibility(View.VISIBLE);
+                } else {
+                    linearLayout.setVisibility(View.INVISIBLE);
+                }
             }
         });
+
     }
 }
